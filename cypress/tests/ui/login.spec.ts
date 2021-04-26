2948e86bd2c58072e113704089c024e17c32255a
Ishaq <ishaqyusuf024@gmail.com>
2021-04-27 00:13:10 +0100

    :tada:

---
+++ cypress/tests/ui/login.spec.ts
@@ -0,0 +1,26 @@
+describe("User Login", function () {
+  beforeEach(() => {});
+
+  const login = (username, password, assert) => {
+    cy.getBySelLike("username").clear().type("ishaqyusuf024@gmail.com");
+    cy.getBySelLike("password").clear().type("admin");
+
+    cy.getBySelLike("submit").should("not.be.disabled");
+    cy.getBySelLike("submit").click();
+    cy.getBySelLike("submit").should("be.disabled");
+    assert();
+  };
+  it("should login successfully", function () {
+    login("ishaqyusuf024@gmail.com", "admin", () => {
+      cy.contains("Login successfull");
+    });
+  });
+  it("login should display correct error", () => {
+    login("ishaqyusuf024@gmail.com", "admin12", () => {
+      cy.contains("Password is incorrect");
+    });
+    login("ishaqyusuf024sakshajs@gmail.com", "admin12", () => {
+      cy.contains("User with email not found on our database");
+    });
+  });
+});
