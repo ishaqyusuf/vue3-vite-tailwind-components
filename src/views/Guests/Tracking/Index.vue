ef9378a5b39244fa3ede0c8597c2a82177c73c71
Ishaq <ishaqyusuf024@gmail.com>
2021-05-15 01:34:36 +0100

    :tada:

--- src/views/Guests/Tracking/Index.vue
+++ src/views/Guests/Tracking/Index.vue
@@ -1,10 +1,13 @@
-<template></template>
+<template>
+  <tracking-section sectional></tracking-section>
+</template>
 
 <script lang="ts">
 import { ref } from "vue";
-
+import TrackingSection from "@/views/Guests/Tracking/TrackingSection.vue";
 export default {
   props: {},
+  components: { TrackingSection },
   setup(props, { emit }) {
     return {};
   },
---
+++ src/views/Guests/Tracking/TrackingSection.vue
@@ -0,0 +1,75 @@
+<template>
+  <div
+    class="flex justify-center items-center relative"
+    :class="[
+      {
+        'x-section sm:h-90h': sectional,
+        'sm:h-30': mini,
+        'gr-esky': !mini,
+      },
+    ]"
+  >
+    <div
+      class="grid grid-cols-5 sm:gap-6"
+      :class="[{ 'w-full lg:w-11/12 xl:w-10/12': !mini }]"
+    >
+      <figure
+        class="hidden sm:block col-span-5 sm:col-span-2 anim overflow-hidden"
+      >
+        <Img class="w-full h-full" no-gradient src="video-thumb_shlk51.jpg" />
+      </figure>
+      <div
+        class="col-span-5 sm:col-span-3 relative bg-yellow-500 xl:bg-transparent"
+      >
+        <div class="absolute all-0 shape-container hidden xl:block"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  props: {
+    sectional: Boolean,
+    mini: Boolean,
+  },
+  setup(props, { emit }) {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.pkg-ka {
+  //   background-repeat: no-repeat;
+  //   background-position: center;
+  background-size: contain;
+  background: url("https://res.cloudinary.com/dba9u9ojd/image/upload/c_scale,h_1000,q_48,w_1000/v1605187082/eaftrack/ilya-cher-XdOvAFE4_RY-unsplash_crmtyh.jpg");
+}
+.shape-container {
+  //   height: 100vh;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #fed300;
+    clip-path: polygon(0 0, 100% 0, 80% 100%, 0 100%);
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    background: #212223;
+    clip-path: polygon(80% 100%, 95% 100%, 80% 0);
+  }
+}
+</style>
