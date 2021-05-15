44cbaa41b57f0f89df49be32e16933bdec249bf8
Ishaq <ishaqyusuf024@gmail.com>
2021-05-15 01:05:13 +0100

    :tada:

--- src/router/basic.routes.ts
+++ src/router/basic.routes.ts
@@ -4,8 +4,19 @@ const routes: Array<RouteRecordRaw> = [
     path: "",
     name: "home",
     component: () => import("../views/Home.vue"),
+    redirect: { name: "tracking" },
   },
-
+  {
+    path: "/tracking",
+    name: "tracking",
+    component: () => import("../views/Guests/Tracking/Index.vue"),
+  },
+  {
+    path: "/tracking/:code",
+    name: "track",
+    component: () => import("../views/Guests/Track/Index.vue"),
+    props: true,
+  },
   {
     path: "/:pathMatch(.*)*",
     name: "NotFound",
